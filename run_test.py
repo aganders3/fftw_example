222c5e6a1e31ab5a6c24248ec2fca2ba37234b4f
Ashley Anderson <aganders3@gmail.com>
2016-03-10 16:13:28 -0700

    Remove PyFI/existing FFT stuff (doesn't matter). The import order is all that matters!

--- run_test.py
+++ run_test.py
@@ -1,13 +1,5 @@
-import fftw_example
 import numpy as np
-
-# import core.math.fft as ft
-# A = np.zeros((4,4), dtype=np.complex64)
-# A[0,:] = 1
-# out_dims = np.array(A.shape, dtype=np.int64)
-
-# ft.fftw(A, out_dims, dim1=0, dim2=1, dir=0)
-
+import fftw_example
 
 A = np.zeros((4,4,4), dtype=np.complex64)
 A[0,:,:] = 1
